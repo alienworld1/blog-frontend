85af2bb3b893329fab2c2509beec1bc275cad0f7
alienworld1 <69631697+alienworld1@users.noreply.github.com>
2024-06-22 21:59:54 +0530

    Add page header and links

---
+++ README.md
@@ -0,0 +1,3 @@
+# Blog
+
+The frontend for my personal blogging website, built with React and Tailwind.
---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Header from './components/header';
+
+function App() {
+  return (
+    <div className="flex flex-col h-screen bg-[#191927] cursor-default">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
